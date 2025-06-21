b27b573f7bde84d50d31c6570d85a08408defc1e
Yash <yash.vyas@dngsoftwares.com>
2025-06-21 16:22:56 +0530

    dummy push

--- src/components/forms/PurchaseRFQ/PurchaseRFQForm.jsx
+++ src/components/forms/PurchaseRFQ/PurchaseRFQForm.jsx
@@ -736,6 +736,7 @@ const PurchaseRFQForm = ({
             value={formData.CompanyName || DEFAULT_COMPANY.label}
           />
         </Grid>
+        {/* changes sales rfq to inquiry */}
         <Grid item xs={12} md={3} sx={{ width: "24%" }}>
           <ReadOnlyField
             label="Inquiry"
